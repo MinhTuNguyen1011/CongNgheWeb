ac70c91ef67477ffc3c0382ccca520264e64df64
Minh Tu <tunm62@wru.vn>
2018-08-02 23:44:45 +0700

    first commit

---
+++ bài tập 5/JQuery.js
@@ -0,0 +1,45 @@
+tendangnhap = document.xuly.tendangnhap.value;
+$matkhau = document.getElementsByName('matkhau');
+lapmatkhau = document.xuly.lapmatkhau.value;
+hoten = document.xuly.hoten.value; 
+diachi = document.xuly.diachi.value;
+dienthoai = document.xuly.dienthoai.value; 
+email = document.xuly.email.value; 
+faceboook = document.xuly.faceboook.value; 
+
+ var emailReg =/[\w.]*[\w]@[\w][\w.]*\.[\w][\w]*/;
+ var regex = new RegExp(emailReg);
+ var $dangky=document.getElementById('dangky');
+ $dangky.addEventListener('click',KiemTra,false);
+
+ function KiemTra(){
+    if(tendangnhap.length==0){
+    alert("Ten dang nhap khong duoc rong.");
+    document.xuly.tendangnhap.focus();
+}
+else if($matkhau.length>20 , $matkhau.length<6){
+    alert('Mat khau phai tu 6 den 20 ki tu');
+    document.xuly.matkhau.focus();
+}
+else if(lapmatkhau!=matkhau){
+    alert('Mat khau khong khop');
+    document.xuly.lapmatkhau.focus();
+}
+else if(hoten.length==0){
+    alert('Ho ten khong duoc rong');
+    document.xuly.hoten.focus();
+}
+else if(diachi.length==0){
+    alert('Dia chi khong duoc rong');
+    document.xuly.diachi.focus();
+}
+else if(dienthoai.length<10, dienthoai.length >11){
+    alert('So dien thoai khong hop le');
+}
+else if(!regex.test(email)){
+    alert("Email khong hop le.");
+    document.xuly.email.focus();
+}
+else 
+alert('Chuc mung ban da tro thanh thanh vien cua website');
+}
